86b6820482649e4722066d6f7f0b0d9c2f9829d6
achmadanval <anvalalan@gmail.com>
2025-06-05 19:41:51 +0700

    kendaraan

---
+++ Kendaraan.java
@@ -0,0 +1,16 @@
+public class Kendaraan {
+
+    String platNomor, tipe, merk;
+
+    Kendaraan(String platNomor, String tipe, String merk){
+        this.platNomor = platNomor;
+        this.tipe = tipe;
+        this.merk = merk;
+    }
+
+    void tampilkanInformasi (){ 
+        System.out.println("Plat Nomor Kendaraan : " + platNomor);
+        System.out.println("Tipe Kendaraan       : " + tipe);
+        System.out.println("Merek Kendaraan      : " + merk);
+    }
+}
